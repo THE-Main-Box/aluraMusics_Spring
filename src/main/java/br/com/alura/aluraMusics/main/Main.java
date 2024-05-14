9a0e6e1444db0c81cc30ddec890cea97686929e3
Thiago de Almeida Barbosa <curiosidades381@gmail.com>
2024-05-14 11:26:57 -0300

    test
    runned a aplication test to see if everything runs properly

---
+++ src/main/java/br/com/alura/aluraMusics/main/Main.java
@@ -0,0 +1,8 @@
+package br.com.alura.aluraMusics.main;
+
+public class Main {
+
+    public Main(){
+        System.out.println("Ola mundo");
+    }
+}
---
+++ src/test/java/br/com/alura/aluraMusics/AluraMusicsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.aluraMusics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraMusicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
